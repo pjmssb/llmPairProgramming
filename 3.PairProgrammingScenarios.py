0441c424685f576d1b54dd929edc03f7ce8629eb
F0r4st3r0 <pjmssb@hotmail.com>
2023-10-05 22:41:21 +0100

    Terminando unidad

--- 3.PairProgrammingScenarios.py
+++ 3.PairProgrammingScenarios.py
@@ -56,23 +56,88 @@ def generate_text(prompt,
 #"""
 
 # option 1
+#prompt_template = """
+#Can you please simplify this code for a linked list in Python? You are an expert on Pythonic code.
+
+#{question}
+
+#Please, comment each line in detail. Explain in detail what you did to modify it, and why.
+#"""
+
+#prompt_template = """
+#Can you please create test cases in code for this Python code?
+
+#{question}
+
+#Explain in detail what these test cases are designed to achieve.
+#"""
+
+
+#prompt_template = """
+#Can you please make this code more efficient?
+
+#{question}
+
+#Explain in detail what you changed and why.
+#"""
+
 prompt_template = """
-Can you please simplify this code for a linked list in Python?
+Can you please help me to debug this code?
 
 {question}
 
-Explain in detail what you did to modify it, and why.
+Explain in detail what you found and why it was a bug.
 """
 
+
+
+#question = """
+#def func_x(array)
+#  for i in range(len(array)):
+#    print(array[i])
+#"""
+
+# I deliberately introduced a bug into this code! Let's see if the LLM can find it.
+# Note -- the model can't see this comment -- but the bug is in the
+# print function. There's a circumstance where nodes can be null, and trying
+# to print them would give a null error.
 question = """
-def func_x(array)
-  for i in range(len(array)):
-    print(array[i])
+class Node:
+   def __init__(self, data):
+      self.data = data
+      self.next = None
+      self.prev = None
+
+class doubly_linked_list:
+   def __init__(self):
+      self.head = None
+
+# Adding data elements
+   def push(self, NewVal):
+      NewNode = Node(NewVal)
+      NewNode.next = self.head
+      if self.head is not None:
+         self.head.prev = NewNode
+      self.head = NewNode
+
+# Print the Doubly Linked list in order
+   def listprint(self, node):
+       print(node.data),
+       last = node
+       node = node.next
+
+dllist = doubly_linked_list()
+dllist.push(12)
+dllist.push(8)
+dllist.push(62)
+dllist.listprint(dllist.head)
+
 """
 
 
 
 completion = generate_text(
-    prompt = prompt_template.format(question=question)
+    prompt = prompt_template.format(question=question),
+    temperature = 0.0
 )
 print(completion.result)
